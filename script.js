9c6b55832ca96773de6442b1e853abe66fd96d88
elisazrgz <elisazaragoza96@gmail.com>
2024-11-01 18:06:26 +0100

    act. creación carrito

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+/* TO DO:
+
+Al hacer clic en “Agregar al Carrito”, el producto debe añadirse a una lista que representa el carrito de compras.
+El carrito debe mostrar el nombre, cantidad, precio unitario, y precio total por producto.
+Debe incluir opciones para aumentar o disminuir la cantidad de un producto y eliminar productos del carrito.
+
+El carrito debe mostrar el total de la compra, actualizado automáticamente al modificar el carrito.
+
+El carrito de compras debe poder mostrarse y ocultarse mediante un botón o icono.
+Debe mostrarse un mensaje o alerta cuando se intente proceder a la compra con un carrito vacío.
+*/
+
 let $container = document.querySelector(".contProducts")
+
+// recorremos el array de productos para imprimir cada píldora:
 
 for(let product of productos){
     let $pill = document.createElement("div");
@@ -28,4 +42,20 @@ for(let product of productos){
     $pill.appendChild($btnAddToBasket);
 
     $container.appendChild($pill);
+}
+
+// -crear un objeto con los productos del carrito
+// -crear un evento click que llame a una funcion por la que se añade el producto al carrito, si ya está dentro sumar otra unidad
+
+
+// creamos la función para que el carrito se muestre u oculte al hacer click:
+
+let $toggleCartButton = document.querySelector("#toggleCartButton");
+$toggleCartButton.addEventListener("click", toggleCart);
+
+function toggleCart(){
+    let $shoppingCart = document.querySelector(".shoppingCart");
+    $shoppingCart.classList.toggle("hidden");
+    // toggle quiere decir que si esa clase está la quita, y si no la añade
+
 }
